1b5098b886f79d5987fa06418c1d948ca5d75717
AD HASAN <adsansapu@gmail.com>
2022-09-21 18:30:00 +0700

    reactcounter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function Counter (props){
+
+  return(
+    <div>
+      <p>Title : {props.title}</p>
+      <h1>Counter:{props.counter}</h1>
+      <button onClick={()=> props.onTambah()}>tambah</button>
+      <button onClick={()=> props.onKurang()}>kurang</button>
     </div>
-  );
+    
+  )
 }
+class Clock extends React.Component {
+  componentWillUnmount(){
+    console.log("componentWillUnmount");
+    
+  }
+  render()
+ 
+  {
+    return<h1>Clock Component</h1>
 
-export default App;
+  }
+
+}
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      title : "To-Do-App",
+      counter : 0,
+      isClock:false,
+
+    };
+   
+    
+  }
+  // componentDidMount(){
+  //   console.log("componentDidMount");
+  // }
+  // componentDidUpdate(){
+  //   console.log("componentDidUpdate");
+  // }
+  handleTambah(){
+    const newCounter = this.state.counter + 1;
+    this.setState({
+      counter:newCounter
+    })
+
+  }
+
+  handleKurang(){
+    const newCounter = this.state.counter - 1;
+    this.setState({
+      counter:newCounter
+    })
+  }
+  handleToggleClock () {
+    this.setState (
+      {
+        isClock:!this.state.isClock,
+      }
+    );
+
+  }
+    render() {
+      console.log("render method");
+      return (
+      <div className='App'>
+        <h1>{this.state.title}</h1>
+        <hr/>
+        < Counter title="Belajar Props" counter={this.state.counter}
+        onTambah={() => this.handleTambah()}
+        onKurang={() => this.handleKurang()}
+        
+        />
+        <hr/>
+        <button onClick={()=>this.handleToggleClock()}>Toggle Clock</button>
+        {this.state.isClock && <Clock/>}
+      </div>
+      );
+    }
+  }
+
+
+
+    
+export default App
